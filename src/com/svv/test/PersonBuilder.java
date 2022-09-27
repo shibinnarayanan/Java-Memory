4430cdb87741f75fbb46017cb615bf602d2281cb
HP Pavilion <HP Pavilion@HPPavilion15.home>
2022-09-27 20:47:00 +0100

    First commit

---
+++ src/com/svv/test/PersonBuilder.java
@@ -0,0 +1,26 @@
+package com.svv.test;
+
+public class PersonBuilder {
+	
+    private static Person buildPerson(int id, String name) {
+        return new Person(id, name);
+    }
+
+    public static void main(String[] args) {
+        int id = 23;
+        String name = "John";
+        Person person= buildPerson(id, name);
+    }
+}
+
+class Person {
+	int id;
+    String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+
